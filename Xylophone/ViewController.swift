f09d48f0da9208eec49fc56ad00e7d69842fee4c
m00racle <yanuar.prakosa@gmail.com>
2021-06-11 10:05:05 +0700

    add sound C from snippet copied Stackoverflow

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
@@ -16,7 +17,31 @@ class ViewController: UIViewController {
 
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        print("key pressed")
+        playSound()
+    }
+    
+    var player:AVAudioPlayer? //set variable named player (type of AVAudioPlayer)
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
     
 
